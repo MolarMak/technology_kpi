5e33608a8eb94cf582b2ae46aede449389e667c1
MolarMak <molarmak@gmail.com>
2019-06-08 02:17:47 +0300

    Add food activity

---
+++ food-tracker-client/app/src/main/java/com/molarmak/foodtracker/AddFoodActivity.kt
@@ -0,0 +1,12 @@
+package com.molarmak.foodtracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddFoodActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_food)
+    }
+}
